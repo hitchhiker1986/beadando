d26b6a210e7626c99dfd1ae5cf6c2763af9caac9
hitchhiker1986 <35505671+hitchhiker1986@users.noreply.github.com>
2023-05-14 22:21:59 +0200

    console.log kiszedve

--- src/function-tester/tests/index.js
+++ src/function-tester/tests/index.js
@@ -2,6 +2,5 @@ import theSimpleExample from "./the-simple-example";
 import theComplexExample from "../../stories/example-data/the-complex-example";
 
 export default [
-    console.log("valami");
     theSimpleExample, theComplexExample
 ];
