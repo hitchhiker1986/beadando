0afb684874585914a34fba4cb09603cda316f1de
hitchhiker1986 <35505671+hitchhiker1986@users.noreply.github.com>
2023-05-14 22:17:58 +0200

    # Please enter the commit message for your changes. Lines starting# with '#' will be ignored, and an empty message aborts the commit.## On branch master# Your branch is up to date with 'origin/master'.## Changes to be committed:#	modified:   src/function-tester/tests/index.js#

--- src/function-tester/tests/index.js
+++ src/function-tester/tests/index.js
@@ -2,5 +2,6 @@ import theSimpleExample from "./the-simple-example";
 import theComplexExample from "../../stories/example-data/the-complex-example";
 
 export default [
+    console.log("valami");
     theSimpleExample, theComplexExample
 ];
